1ba580c0cbc5ea550fb9b6891e993faf1ba5228c
Your Name <you@example.com>
2019-10-31 14:32:13 +0800

    application for film

---
+++ app/src/main/java/com/lifh/imagehut/imagehut/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lifh.imagehut.imagehut.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lifh.imagehut.imagehut.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
